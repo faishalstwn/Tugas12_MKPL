f599dd00cce1935303e79bbfa58373102c2c5e35
faishalstwn <169064956+faishalstwn@users.noreply.github.com>
2026-05-29 17:36:59 +0700

    refactor: improve Counter logic and fix incorrect exponent operation

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -1,52 +1,57 @@
-
-
 public class Counter {
 
-	private int count;
-	
-	public Counter() {
-		this.reset();
-	}
-		
-	public void reset() {
-		count = 0;
-	}
-	
-	public void increment() {
-		count++;
-	}
-	
-	public void increaseBy(int i) {
-		count+=i;
-	}
-	
-	public void decrement() {
-		count--;
-	}
-	
-	public void decreaseBy(int i) {
-		count-=i;
-	}
-	
-	public void multiplyBy(int i){
-		count = count * i;
-	}
-	
-	public void triple(){
-		int i = 3;
-		multiplyBy(i);
-	}
+    private int count;
 
-	public void powerBy(int i){
-		count = count ^ i;
-	}
-	
-	public boolean isCountEven(){
-		return count%2 == 0;
-	}
-	
-	public int getCount() {
-		return count;
-	}
-	
+    public Counter() {
+        this.reset();
+    }
+
+    public void reset() {
+        count = 0;
+    }
+
+    public void increment() {
+        count++;
+    }
+
+    public void increaseBy(int i) {
+        count += i;
+    }
+
+    public void decrement() {
+        count--;
+    }
+
+    public void decreaseBy(int i) {
+        count -= i;
+    }
+
+    public void multiplyBy(int i) {
+        count *= i;
+    }
+
+    public void triple() {
+        multiplyBy(3);
+    }
+
+    // FIX: sebelumnya XOR (^), sekarang benar-benar pangkat
+    public void powerBy(int i) {
+        if (i < 0) {
+            throw new IllegalArgumentException("Exponent tidak boleh negatif");
+        }
+
+        int result = 1;
+        for (int j = 0; j < i; j++) {
+            result *= count;
+        }
+        count = result;
+    }
+
+    public boolean isCountEven() {
+        return count % 2 == 0;
+    }
+
+    public int getCount() {
+        return count;
+    }
 }
