4458c8756387006b7044451845b2b0b80f4e2447
faishalstwn <169064956+faishalstwn@users.noreply.github.com>
2026-05-29 17:55:34 +0700

    refactor: simplify Driver output using helper method

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,20 +1,19 @@
-
 public class Driver {
 
-	public static void main(String[] args) {
-		
-		Counter counter = new Counter();
-		
-		System.out.println("Current count:" + counter.getCount());
-		
-		counter.increment();
-		
-		System.out.println("Current count:" + counter.getCount());
-		
-		counter.decrement();
-		
-		System.out.println("Current count:" + counter.getCount());
-		
-	}
+    public static void main(String[] args) {
 
+        Counter counter = new Counter();
+
+        printCount("Initial count", counter);
+
+        counter.increment();
+        printCount("After increment", counter);
+
+        counter.decrement();
+        printCount("After decrement", counter);
+    }
+
+    private static void printCount(String label, Counter counter) {
+        System.out.println(label + ": " + counter.getCount());
+    }
 }
